2d391119bc1c029f283244bdfc17ec7d8a241009
nakama <nakama@baguet.org>
2022-08-03 00:16:01 +0200

    rice_me.sh added

---
+++ rice_me.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+
+### OPTIONS AND VARIABLES ###
+
+### FUNCTIONS ###
+
+installpkg() {
+	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
+}
+
+error() {
+	# Log to stderr and exit with failure.
+	printf "%s\n" "$1" >&2
+	exit 1
+}
+
+### SCRIPT ###
+
+# Check if user is root. Install whiptail.
+pacman --noconfirm --needed -Sy libnewt ||
+	error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
