c99ca52c628a6513c96ff1fed053164b86ae0849
nakama <nakama@baguet.org>
2022-08-03 13:10:18 +0200

    Updated rice script - new functions

--- rice_me.sh
+++ rice_me.sh
@@ -1,21 +1,38 @@
 #/bin/sh
+#
+# Run this script as any user on the system with sudo privileges
+#
 
 ### OPTIONS AND VARIABLES ###
 
 ### FUNCTIONS ###
 
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+ 
 installpkg() {
 	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
 }
 
 error() {
-	# Log to stderr and exit with failure.
+	# Log to stderr and exit with failure
 	printf "%s\n" "$1" >&2
 	exit 1
 }
 
+main() {
+	# Check OS distribution
+	command_exists pacman ||
+		error "Are you sure you're running this on an Arch-based distribution ?"
+
+	# Check if user is root. Install whiptail.
+	pacman --noconfirm --needed -Sy libnewt >/dev/null 2>&1 ||
+		error "Are you sure you're running this as the root user, have an internet connection ?"
+	
+
+}
+
 ### SCRIPT ###
 
-# Check if user is root. Install whiptail.
-pacman --noconfirm --needed -Sy libnewt ||
-	error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
+main
