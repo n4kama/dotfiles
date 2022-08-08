ea8eaf79cf03113302f2ae5cb286eccfb3bc3349
nakama <nakama@baguet.org>
2022-08-09 00:06:08 +0200

    Bugs fixed

--- rice_me.sh
+++ rice_me.sh
@@ -41,7 +41,7 @@ install_yay() {
 	&& git clone https://aur.archlinux.org/yay.git \
 	&& chown -R $USERNAME:$USERNAME ./yay \
 	&& cd yay \
-	&& sudo -u $USERNAME makepkg -si --noconfirm
+	&& sudo -u $USERNAME makepkg -si --noconfirm >/dev/null 2>&1
 	echo "[install_yay] yay has been installed in /opt"
 }
 
